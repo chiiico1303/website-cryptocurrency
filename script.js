7439307278090504e72560f530b932397b0bd532
chiiico1303 <fsalesbezerra@gmail.com>
2022-11-11 16:08:12 -0300

    change currency brl

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var doge = document.getElementById("dogecoin")
 var settings = {
   async: true,
   scrossDomain: true,
-  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=brl",
   method: "GET",
   headers: {},
 }
 $.ajax(settings).done(function (response) {
-  btc.innerHTML = response.bitcoin.usd
-  eth.innerHTML = response.ethereum.usd
-  doge.innerHTML = response.dogecoin.usd
+  btc.innerHTML = response.bitcoin.brl
+  eth.innerHTML = response.ethereum.brl
+  doge.innerHTML = response.dogecoin.brl
 })
